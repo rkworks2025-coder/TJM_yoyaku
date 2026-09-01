64dc59ce72641454a62075314cd10e08526d46b9
Ryu.ktym <226134443+rkworks2025-coder@users.noreply.github.com>
2026-09-01 17:18:26 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ if NEEDS_SCRAPE:
     from selenium.webdriver.common.keys import Keys
 
 # --- Discord通知用設定 ---
-DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/REDACTED/REDACTED"
+DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL", "")
 
 def send_discord_notification(message):
     if not DISCORD_WEBHOOK_URL: return
